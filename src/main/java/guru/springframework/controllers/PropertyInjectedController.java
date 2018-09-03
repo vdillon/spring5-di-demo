fc83675d1a268cf007e1e76653bd232b5a53100f
Van Dillon <vdillon@edge2web.com>
2018-09-03 11:05:24 -0400

    Fixed a few errors

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -14,6 +14,7 @@ public class PropertyInjectedController {
 
     @Autowired
     @Qualifier("greetingServiceImpl")
+    // If qualifier removed, Primary will be used, even if the property name is the same as a bean!
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
