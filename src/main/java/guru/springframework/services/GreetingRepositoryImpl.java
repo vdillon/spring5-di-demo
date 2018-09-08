245afdeb7b9ea5825ef2e8d2a341d6407aa85718
Van Dillon <vdillon@edge2web.com>
2018-09-08 15:43:26 -0400

    Fixed a few errors

--- src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -1,11 +1,11 @@
 package guru.springframework.services;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 5/24/17.
  */
-@Service
+@Component
 public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
