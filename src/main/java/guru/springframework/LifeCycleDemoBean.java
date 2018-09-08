1414facaad18eb2d3ed5a06b43c7c1a8dbe5ba66
Van Dillon <vdillon@edge2web.com>
2018-09-08 15:59:35 -0400

    Fixed a few errors

--- src/main/java/guru/springframework/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/LifeCycleDemoBean.java
@@ -14,23 +14,15 @@ import javax.annotation.PreDestroy;
  */
 @Component
 public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
-        BeanFactoryAware, ApplicationContextAware{
-
+                                          BeanFactoryAware, ApplicationContextAware {
 
     public LifeCycleDemoBean() {
         System.out.println("## I'm in the LifeCycleBean Constructor");
     }
 
     @Override
-    public void destroy() throws Exception {
-        System.out.println("## The Lifecycle bean has been terminated");
-
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("## The LifeCycleBean has its properties set!");
-
+    public void setBeanName(String name) {
+        System.out.println("## My Bean Name is: " + name);
     }
 
     @Override
@@ -39,14 +31,12 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
     }
 
     @Override
-    public void setBeanName(String name) {
-        System.out.println("## My Bean Name is: " + name);
-
-    }
-
-    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("## Application context has been set");
+    }
+
+    public void beforeInit(){
+        System.out.println("## - Before Init - Called by Bean Post Processor");
     }
 
     @PostConstruct
@@ -54,16 +44,22 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
         System.out.println("## The Post Construct annotated method has been called");
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## The LifeCycleBean has its properties set!");
+    }
+
+    public void afterInit(){
+        System.out.println("## - After init called by Bean Post Processor");
+    }
+
     @PreDestroy
     public void preDestroy() {
         System.out.println("## The Predestroy annotated method has been called");
     }
 
-    public void beforeInit(){
-        System.out.println("## - Before Init - Called by Bean Post Processor");
-    }
-
-    public void afterInit(){
-        System.out.println("## - After init called by Bean Post Processor");
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("## The Lifecycle bean has been terminated");
     }
 }
